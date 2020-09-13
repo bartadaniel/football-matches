4260587b9b5f4fdab6b0dc0a9eb0401eb3e60b0b
Daniel Barta <barta.daniel@gmail.com>
2020-09-13 17:59:00 +0200

    Display all match highlights in match cards

--- src/app/match-feed/match-feed.component.spec.ts
+++ src/app/match-feed/match-feed.component.spec.ts
@@ -1,19 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
+import { MatchCardComponent } from '../match-card/match-card.component';
+import { MatchHighlight } from '../models/match-highlight.model';
+import { createMockMatchHighlight } from '../models/match-highlight.model.mock';
 import { MatchFeedComponent } from './match-feed.component';
 
 describe('MatchFeedComponent', () => {
   let component: MatchFeedComponent;
   let fixture: ComponentFixture<MatchFeedComponent>;
+  let mockMatchHighlights: MatchHighlight[];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MatchFeedComponent ]
-    })
-    .compileComponents();
+      declarations: [MatchFeedComponent, MatchCardComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
+    mockMatchHighlights = [
+      createMockMatchHighlight(1),
+      createMockMatchHighlight(2),
+    ];
     fixture = TestBed.createComponent(MatchFeedComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -21,5 +28,17 @@ describe('MatchFeedComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display match highlights in cards', () => {
+    component.matchHighlights = mockMatchHighlights;
+    fixture.detectChanges();
+    const displayedMatchCards = fixture.debugElement
+      .queryAll(By.directive(MatchCardComponent))
+      .map(
+        (matchCard) =>
+          (matchCard.componentInstance as MatchCardComponent).matchHighlight
+      );
+    expect(displayedMatchCards).toEqual(mockMatchHighlights);
   });
 });
--- src/app/match-feed/match-feed.component.ts
+++ src/app/match-feed/match-feed.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { MatchHighlight } from '../models/match-highlight.model';
 
 @Component({
   selector: 'app-match-feed',
   templateUrl: './match-feed.component.html',
-  styleUrls: ['./match-feed.component.css']
+  styleUrls: ['./match-feed.component.css'],
 })
 export class MatchFeedComponent implements OnInit {
+  matchHighlights: MatchHighlight[];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
