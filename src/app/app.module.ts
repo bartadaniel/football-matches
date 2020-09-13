758a679ff7f92717a1f610c3917c09b9c9deac8c
Daniel Barta <barta.daniel@gmail.com>
2020-09-13 17:50:26 +0200

    Format app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
-import { EffectsModule } from '@ngrx/effects';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
 import { MatchFeedComponent } from './match-feed/match-feed.component';
+import { metaReducers, reducers } from './reducers';
+import { MatchCardComponent } from './match-card/match-card.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MatchFeedComponent
-  ],
+  declarations: [AppComponent, MatchFeedComponent, MatchCardComponent],
   imports: [
     BrowserModule,
     StoreModule.forRoot({}, {}),
     StoreModule.forRoot(reducers, {
-      metaReducers
+      metaReducers,
     }),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
     EffectsModule.forRoot([]),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
